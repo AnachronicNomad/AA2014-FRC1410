5f279c336b08ecc76dd586ad9085579360978516
Samuel Chen <fymfam31415@gmail.com>
2014-03-14 13:45:48 -0600

    Remove uneeded Auto commands/NT
    removed commented out commands that were maintained for Hot goal
    determination and the results, however, no longer needed with the change
    to not do Hot goal determination

--- AA2014-FRC1410/CommandBasedRobot.cpp
+++ AA2014-FRC1410/CommandBasedRobot.cpp
@@ -2,54 +2,22 @@
 #include "Commands/Command.h"
 #include "CommandBase.h"
 #include "Commands/Autonomous/HotFirst.h"
-#include "Commands/Autonomous/NotHotFirst.h"
-#include "Commands/Autonomous/PreAutoGroup.h"
 
 class CommandBasedRobot : public IterativeRobot {
 private:
-	//Command *autonomousCommand;
+
 	LiveWindow *lw;
-	NetworkTable *table;
-	Command *hotAutoCom;
-	Command *notHotAutoCom;
-	Command *preAutoCom;
+	//Command *hotAutoCom;
+
 	
 	virtual void RobotInit() {
 		CommandBase::init();
-		//autonomousCommand = new Command();
-		hotAutoCom = new HotFirst();
-		notHotAutoCom = new NotHotFirst();
-		preAutoCom = new PreAutoGroup();
+		//hotAutoCom = new HotFirst();
 		lw = LiveWindow::GetInstance();
-		table = NetworkTable::GetTable("SmartDashboard");
 	}
 	
 	virtual void AutonomousInit() {
-		preAutoCom->Start();
-		while(preAutoCom->IsRunning() == true)
-		{
-			printf("Running Inital Setup for Auto\n");
-		}
-		if(preAutoCom->IsRunning() == false)
-		{
-			if(table->GetBoolean("Target", false))
-						{
-							if(table->GetBoolean("Hot", false))
-							{
-								printf("Auto Hot\n");
-								hotAutoCom->Start();
-							}
-							else
-							{
-								printf("Auto Not Hot\n");
-								notHotAutoCom->Start();
-							}
-						}
-						else
-						{
-							printf("No Target\n");
-						}
-		}
+		//hotAutoCom->Start();
 	}
 	
 	virtual void AutonomousPeriodic() {
@@ -62,8 +30,6 @@ private:
 		// continue until interrupted by another command, remove
 		// this line or comment it out.
 		//autonomousCommand->Cancel();
-		hotAutoCom->Cancel();
-		notHotAutoCom->Cancel();
 	}
 	
 	virtual void TeleopPeriodic() {
